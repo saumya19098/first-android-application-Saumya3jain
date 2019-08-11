83b72ec9c00ae28e7280dba82b1df0c802a26786
Saumya jain <53992526+Saumya3jain@users.noreply.github.com>
2019-08-11 14:36:56 +0530

    Add files via upload
    Solutions

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view)
+    {
+        Log.d( "MYCODE","IT WORKS");
+        Intent newIntent = new Intent(this,Main2Activity.class);
+        startActivity(newIntent);
+    }
+}
